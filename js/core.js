769d4f94f56047763f4ecc073876aab8dfbb6b67
melisova111 <aytunukmirlanovna@gmail.com>
2024-01-31 22:46:27 +0600

    add <section class="advan">

---
+++ js/core.js
@@ -0,0 +1,45 @@
+// // slider
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView:3,
+//     spaceBetween: 30,
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+//     pagination: {
+//         el: ".swiper-pagination",
+//     },
+//     mousewheel: true,
+//     keyboard: true,
+// });
+
+// swiper.slideNext();
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView:1,
+//     spaceBetween: 30,
+//     breakpoints:{
+//         320:{
+//             slidesPerView:2,
+//             spaceBetween: 20,
+//         },
+//         480:{
+//             slidesPerView:3,
+//             spaceBetween: 30,
+//         },  
+//         640:{
+//             slidesPerView:4,
+//             spaceBetween: 40,
+//         },
+//     },
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+//     pagination: {
+//         el: ".swiper-pagination",
+//     },
+//     mousewheel: true,
+//     keyboard: true,
+// });
+  
+  
