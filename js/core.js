bb66f19f82ca2a961f787486879c2cb3c91227aa
melisova111 <aytunukmirlanovna@gmail.com>
2024-02-06 21:01:14 +0600

    add swiper

--- js/core.js
+++ js/core.js
@@ -1,24 +1,41 @@
-// {/* <script>
-// // Get the video
-// var video = document.getElementById("myVideo");
 
-// // Get the button
-// var btn = document.getElementById("myBtn");
-
-// // Pause and play the video, and change the button text
-// function myFunction() {
-//     if (video.paused) {
-//         video.play();
-//         btn.innerHTML = "Pause";
-//     } else {
-//         video.pause();
-//         btn.innerHTML = "Play";
-//     }
-// }
-// </script> */}
 const images = document.querySelectorAll('.main_img');
 
 
 window.addEventListener('scroll', () => {
     images.style.right = "-" + (window.scrollX / 1.5) + "px";
 })
+
+
+
+
+
+
+
+let currentIndex = 0;
+  const slides = document.querySelectorAll('.slide');
+  const totalSlides = slides.length;
+
+
+  function showSlide(index) {
+    if (index < 0) {
+      currentIndex = totalSlides - 1;
+    } else if (index >= totalSlides) {
+      currentIndex = 0;
+    } else {
+      currentIndex = index;
+    }
+
+    const transformValue = -currentIndex * 30 + '%';
+    document.querySelector('.slider').style.transform = 'translateX(' + transformValue + ')';
+  }
+
+  function prevSlide() {
+    showSlide(currentIndex - 1);
+  }
+
+  function nextSlide() {
+    showSlide(currentIndex + 1);
+  }
+
+    
